cd7f4666d3f77641cac74e1be7b27b881e4dffd9
Moonphy <337465068@qq.com>
2017-01-02 20:32:18 +0800

    first commit

---
+++ myClock/js/my_click.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Moonphy on 17/1/2.
+ */
+function my_click(el){
+    var today = new Date();
+
+    var h = today.getHours(),
+        m = today.getMinutes(),
+        s = today.getSeconds();
+
+    m = m >= 10 ? m : '0' + m;
+    s = s >= 10 ? s : '0' + s;
+
+    el.innerHTML = h + ":" + m + ":" + s;
+
+    setTimeout(function () {
+        my_click(el)
+    }, 1000);
+}
+
+var clock_div = document.getElementById('clock_div');
+
+my_click(clock_div);
+
